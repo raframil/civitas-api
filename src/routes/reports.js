dd10a67e1b6b86cb59d633b16ae058207b4a55e2
Rafael Framil <raframil@gmail.com>
2019-11-18 20:19:39 -0300

    delete report

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -2,7 +2,9 @@ const express = require('express');
 const uuidv4 = require('uuid/v4');
 const Report = require('../models/report');
 const router = express.Router();
+const fs = require('fs');
 
+// Create report
 router.post('/', async (req, res) => {
   try {
     if (!req.files) {
@@ -38,6 +40,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Return all reports
 router.get('/', async (req, res) => {
   try {
     const reports = await Report.find();
@@ -47,12 +50,33 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Return one report
 router.get('/:reportId', async (req, res) => {
   try {
-    const report = await Report.findById(req.params.reportId);
+    const report = await Report.findById({ _id: req.params.reportId });
     return res.status(200).send({ report });
   } catch (err) {
     return res.status(400).send({ error: err });
+  }
+});
+
+// Delete report
+router.delete('/:reportId', async (req, res) => {
+  try {
+    const report = await Report.findById({ _id: req.params.reportId });
+    if (report) {
+      await Report.deleteOne({ _id: req.params.reportId });
+
+      fs.unlink(report.photo, err => {
+        if (err) return res.status(400).json({ error: err });
+      });
+
+      return res.status(200).json({ message: 'Report deleted successfully', report: report });
+    } else {
+      return res.status(404).json({ message: 'Report not found' });
+    }
+  } catch (err) {
+    return res.status(400).json({ error: err });
   }
 });
 
